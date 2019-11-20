729cd5d1db1c33ad556a0b0c0b878ab9b121182e
Walter Johnson <johnwalt@oregonstate.edu>
2019-11-19 21:54:45 -0800

    first commit

---
+++ forktest.c
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    pid_t spawnpid = -5;
+    int ten = 10;
+
+    spawnpid = fork();
+    switch (spawnpid)
+    {
+        case -1:
+            perror("Hull Breach!");
+            exit(1);
+            break;
+        case 0:
+            ten = ten + 1;
+            printf("I am the child! ten = %d\n", ten);
+            break;
+        default:
+            ten = ten - 1;
+            printf("I am the parent! ten = %d\n", ten);
+            break;
+    }
+    printf("This will be executed by both of us!\n");
+}
